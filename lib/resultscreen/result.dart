936f07100e79cbc5088f1d72483fe9de523eb7ae
vc174887 <vc174887@fh-muenster.de>
2022-03-01 09:37:45 +0100

    statistiken fertig. todo: multi ansicht der statistiken

--- lib/resultscreen/result.dart
+++ lib/resultscreen/result.dart
@@ -1,4 +1,6 @@
+import 'package:advanced_quiz_app/homescreen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ResultScreen extends StatefulWidget {
   int totalAmountOfCorrectAnswers;
@@ -14,24 +16,71 @@ class ResultScreen extends StatefulWidget {
 class _ResultState extends State<ResultScreen> {
   int totalAmountOfCorrectAnswers;
   int totalAmountOfQuestions;
+  late List<Statistics> _statistics;
+
+  @override
+  void initState() {
+    _statistics = getStatistics();
+    super.initState();
+  }
 
   _ResultState(this.totalAmountOfQuestions, this.totalAmountOfCorrectAnswers);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Quiz"),
-        centerTitle: true,
-      ),
-      body: Container(
-        color: Colors.lightGreen,
-        child: Text("Sie haben: " +
-            this.totalAmountOfCorrectAnswers.toString() +
-            " von " +
-            this.totalAmountOfQuestions.toString() +
-            " Antworten richtig"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz"),
+          centerTitle: true,
+        ),
+        body: SfCircularChart(
+          backgroundColor: HomeState.global_background_color,
+          title: ChartTitle(
+            text: 'Statistiken',
+            textStyle: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          legend: Legend(
+            isVisible: true,
+            overflowMode: LegendItemOverflowMode.wrap,
+          ),
+          series: <CircularSeries>[
+            PieSeries<Statistics, String>(
+              //PieSeries -> DoughnutSeries
+              //PieSeries -> RadialBarSeries
+              dataSource: _statistics,
+              xValueMapper: (Statistics data, _) => data.label,
+              yValueMapper: (Statistics data, _) => data.amount,
+              dataLabelSettings: DataLabelSettings(
+                isVisible: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  List<Statistics> getStatistics() {
+    final List<Statistics> statistics = [
+      Statistics(
+        "richtig",
+        this.totalAmountOfCorrectAnswers,
+      ),
+      Statistics(
+        "falsch",
+        this.totalAmountOfQuestions - this.totalAmountOfCorrectAnswers,
+      )
+    ];
+    return statistics;
+  }
+}
+
+class Statistics {
+  final String label;
+  final int amount;
+
+  Statistics(this.label, this.amount);
 }
